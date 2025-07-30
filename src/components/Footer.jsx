7c72b10b5a4b2323f704aeb60f53fa4aacae0bfb
kartik11m <kartikmaheshwari698@gmail.com>
2025-07-30 19:28:24 +0530

    using darkMode state

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 
-function Footer() {
+function Footer({darkMode , setDarkMode}) {
   const year = new Date().getFullYear();
   return (
-    <footer className="text-center bg-gray-200">
+    <footer className={darkMode ? "text-center bg-gray-600 text-white py-2" : "text-center bg-gray-200 py-2"}>
       <p>Copyright ⓒ {year}</p>
     </footer>
   );
